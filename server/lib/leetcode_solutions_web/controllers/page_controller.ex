37e376dfdde663bb779392842f440571b91266b7
Feifan Zhou <feifan@me.com>
2019-07-11 20:06:14 -0700

    Render root README on homepage

--- server/lib/leetcode_solutions_web/controllers/page_controller.ex
+++ server/lib/leetcode_solutions_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule LeetcodeSolutionsWeb.PageController do
   use LeetcodeSolutionsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    case File.read(Path.relative_to_cwd("../README.md")) do
+      {:ok, markdown} -> html(conn, Earmark.as_html!(markdown))
+      {:error, reason} -> text(conn, "Error reading root readme: " <> Atom.to_string(reason))
+    end
   end
 end
